3c4d69dfd57e0cef27efcdbc18a2b676405188e1
rbgavito <32744667+rbgavito@users.noreply.github.com>
2017-10-12 17:46:26 +0100

    Create __init__.py

---
+++ IVsweeper/Keithley/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""Keithley package
+    
+Package including the sweep and data manipulation functions
+    for different Keithley source measure units.
+
+Currently supported SMUs are Keithley 2602B, Keithley 2400,
+    and Keithley 6430.
+
+Further documentation can be found at each specific module:
+    - K2400.py
+    - K2602B.py
+    - K6430.py
+"""
+
+__author__ = "Ramón Bernardo Gavito, and Benjamin Astbury"
+__copyright__ = "Copyright 2017, Young-Quantum Group, Lancaster University"
+__credits__ = ["Ramón Bernardo Gavito", "Benjamin Astbury", "Hamzah Shokeir",
+               "Thomas McGrath", "Robert J. Young"]
+__license__ = "GPL"
+__version__ = "0.0.1"
+__maintainer__ = "Ramón Bernardo Gavito"
+__email__ = "r.bernardogavito1@lancaster.ac.uk"
+__status__ = "Development"
